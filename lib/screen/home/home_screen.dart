929774595e30d27c24f16f4b7231ac6ad953a7f6
Felipe Marques <equipeffoficial@gmail.com>
2021-01-10 19:51:13 -0400

    Construindo tela de login .

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -3,8 +3,45 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Entrar"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Form(
+              child: ListView(
+                padding: const EdgeInsets.all(16),
+                shrinkWrap: true,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(hintText: "E-mail"),
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    decoration: const InputDecoration(hintText: "Senha"),
+                    autocorrect: false,
+                    obscureText: true,
+                    validator: (pass) {
+                      if (pass.isEmpty || pass.length < 6)
+                        return "Senha Inválida";
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  RaisedButton(
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
